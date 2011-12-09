432dc6a49df723fd1587e945cf7a850c1b7aaca5
realn0whereman <trainstroker@gmail.com>
2011-12-09 03:50:56 -0500

    Added test project

---
+++ MiniRe/ParserGeneratorTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ParserGenerator;
+namespace ParserGeneratorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParserGenerator.ParserGenerator pg = new ParserGenerator.ParserGenerator();
+            //pg.parseGrammar();
+            //while(true);
+        }
+    }
+}
